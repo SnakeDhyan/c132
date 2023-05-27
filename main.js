2e58851baf342c6ab5a1c264f48c5cc7d83c43a3
SnakeDhyan <98876616+SnakeDhyan@users.noreply.github.com>
2023-05-27 10:20:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+img="";
+status="";
+function preload(){
+    img=loadImage('dog_cat.jpg');
+}
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status = Detecting Objects";
+}
+function draw(){
+    image(img,0,0,640,420);
+    fill("#0000FF");
+    text("Dog",45,75);
+    noFill();
+    stroke("#0000FF");
+    rect(30,60,450,350);
+
+    fill("#0000FF");
+    text("Cat",320,120);
+    noFill();
+    stroke("#0000FF");
+    rect(300,90,270,320);
+}
+function modelLoaded(){
+    console.log("Model Loaded!!");
+    status=true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    if(result){
+        console.log(results);
+    }
+}
